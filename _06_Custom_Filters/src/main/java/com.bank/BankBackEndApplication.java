6cd241b408429c82d00bd3fccef5aa04de0e2d2e
Rajan Singh <rjnkumar628@gmail.com>
2025-02-05 09:04:11 +0530

    Custom Filters in spring security

---
+++ _06_Custom_Filters/src/main/java/com.bank/BankBackEndApplication.java
@@ -0,0 +1,21 @@
+package com.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@SpringBootApplication
+
+//@EnableWebSecurity -- Optional in spring boot
+/*@EntityScan("com.bank.model")
+@EnableJpaRepositories("com.bank.repository")*/
+@EnableWebSecurity(debug = true)
+public class BankBackEndApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BankBackEndApplication.class, args);
+	}
+
+}
